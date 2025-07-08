934fe6cee68aca460199e31feb9a59d245081c15
Dimitar Dimitrov <dimitrov.dna@gmail.com>
2025-07-08 22:20:49 +0200

    string padding

--- StringManipulation/StringPadding/Program.cs
+++ StringManipulation/StringPadding/Program.cs
@@ -2,12 +2,40 @@
 {
     public static void Main(string[] args)
     {
-        ex1();
+        ex2();
     }
     static void ex1()
     {
-        string firstName = "John";
-        string lastName = "Doe";
-        Console.WriteLine($"My name is {firstName} {lastName}.");
+        string input = "Pad this";
+        Console.WriteLine(input.PadLeft(12));
+        Console.WriteLine(input.PadRight(12, 'L'));
+    }
+
+    static void ex2()
+    {
+        string customerName = "Ms. Barros";
+
+        string currentProduct = "Magic Yield";
+        int currentShares = 2975000;
+        decimal currentReturn = 0.1275m;
+        decimal currentProfit = 55000000.0m;
+
+        string newProduct = "Glorious Future";
+        decimal newReturn = 0.13125m;
+        decimal newProfit = 63000000.0m;
+
+        Console.WriteLine($"Dear {customerName}");
+        Console.WriteLine($"As a customer of our {currentProduct}  offering we are excited to tell you about a new financial product that would dramatically increase your return.\n");
+        Console.WriteLine($"Currently, you own {currentShares:N} sjares at a return of {currentReturn:P}\n");
+        Console.WriteLine($"Our new product, {newProduct} offers a return of {newReturn:P}. Given your current volume, your potential profit would be {newProfit:C}.\n");
+
+        Console.WriteLine("Here's a quick comparison:\n");
+
+        string comparisonMessage = "";
+
+        comparisonMessage += currentProduct.PadRight(20) + currentReturn.ToString("P").PadRight(10) + currentProfit.ToString("C") + "\n";
+        comparisonMessage += newProduct.PadRight(20) + newReturn.ToString("P").PadRight(10) + newProfit.ToString("C") + "\n";
+
+        Console.WriteLine(comparisonMessage);
     }
 }
---
+++ obsidian-csharp-notes/StringManipulation/StringPadding.md
@@ -0,0 +1,5 @@
+- Methods that add blank spaces for formatting purposes (`PadLeft()`, `PadRight()`)
+- Methods that compare two strings or facilitate comparison (`Trim()`, `TrimStart()`, `TrimEnd()`, `GetHashcode()`, the `Length` property)
+- Methods that help you determine what's inside of a string, or even retrieve just a part of the string (`Contains()`, `StartsWith()`, `EndsWith()`, `Substring()`)
+- Methods that change the content of the string by replacing, inserting, or removing parts (`Replace()`, `Insert()`, `Remove()`)
+- Methods that turn a string into an array of strings or characters (`Split()`, `ToCharArray()`)
