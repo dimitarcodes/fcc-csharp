2ec8d8cb943ab9bedd0b0f8e7e55802bd95e44ab
Dimitar Dimitrov <dimitrov.dna@gmail.com>
2025-07-08 14:35:44 +0200

    string exercises

---
+++ stringFormat/StringFormatting.cs
@@ -0,0 +1,30 @@
+﻿class StringFormatting
+{
+    static void Main(string[] args)
+    {
+        ex2();
+    }
+
+    static void ex1()
+    {
+        string firstName = "John";
+        string lastName = "Doe";
+        Console.WriteLine($"My name is {firstName} {lastName}.");
+    }
+
+    static void ex2() {
+        string first = "Hello";
+        string second = "World";
+        Console.WriteLine($"{first} {second}!");
+        Console.WriteLine($"{second} {first}!");
+        Console.WriteLine($"{first} {first} {first}!");
+    }
+
+    static void ex3() {
+        string first = "Hello";
+        string second = "World";
+        Console.WriteLine("{1} {0}!", first, second);
+        Console.WriteLine("{0} {0} {0}!", first, second);
+    }
+
+}
---
+++ stringFormat/StringPadding.cs
@@ -0,0 +1,15 @@
+class StringPadding
+{
+    public static void Main(string[] args)
+    {
+        Example1();
+        // Example2();
+        // Example3();
+    }
+    static void Example1()
+    {
+        string firstName = "John";
+        string lastName = "Doe";
+        Console.WriteLine($"My name is {firstName} {lastName}.");
+    }
+}
